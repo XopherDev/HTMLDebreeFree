aaf17a84490ad6334b222ddd3bf59300a153be4d
Xopher <tessay1@hotmail.com>
2019-03-01 13:44:28 -0700

    Updated layout and include top logo

---
+++ js/dashboard.js
@@ -0,0 +1,23 @@
+﻿function GoToJob() {
+    $('#bodyId').addClass('animated fadeOutUp');
+    $('#bodyId').fadeOut(300);
+
+    var delayInMilliseconds = 300; //1/2 of a second
+
+    setTimeout(function () {
+        $.ajax({
+            url: 'jobs.html',
+            type: "GET",
+            success: function (partialView) {
+                $('#bodyId').removeClass('animated fadeOutUp');
+                $('#bodyId').fadeIn(500);
+                $('#bodyId').addClass('animated slideInUp');
+                $('#bodyId').html(partialView);
+            }
+        });
+    }, delayInMilliseconds);
+}
+
+function GoToJobs() {
+    window.location.href = "../jobs.html";
+}
---
+++ js/login.js
@@ -0,0 +1,109 @@
+﻿function GoToLogin() {
+
+    $('#bodyId').addClass('animated slideOutLeft');
+    $('#bodyId').fadeOut(200);
+
+    var delayInMilliseconds = 200; //1/2 of a second
+
+    setTimeout(function () {
+        $.ajax({
+            url: '../Login/GoToLogin',
+            type: "GET",
+            success: function (partialView) {
+                $('#bodyId').removeClass('animated slideOutLeft');
+                $('#bodyId').fadeIn(500);
+                $('#bodyId').addClass('animated slideInRight');
+                $('#bodyId').html(partialView);
+            }
+        });
+    }, delayInMilliseconds);
+   
+}
+
+function RegisterContactInfo() {
+    $('#bodyId').addClass('animated slideOutLeft');
+    $('#bodyId').fadeOut(200);
+
+    var delayInMilliseconds = 200; //1/2 of a second
+
+    setTimeout(function () {
+        $.ajax({
+            url: '../Login/RegisterContactInfo',
+            type: "GET",
+            success: function (partialView) {
+                $('#bodyId').removeClass('animated slideOutLeft');
+                $('#bodyId').fadeIn(500);
+                $('#bodyId').addClass('animated slideInRight');
+                $('#bodyId').html(partialView);
+            }
+        });
+    }, delayInMilliseconds);
+
+}
+
+function GoToDashboard() {
+    $('#bodyId').addClass('animated fadeOutUp');
+    $('#bodyId').fadeOut(300);
+
+    var delayInMilliseconds = 300; //1/2 of a second
+
+    setTimeout(function () {
+        $.ajax({
+            url: '../Dashboard/GoToDashboard',
+            type: "GET",
+            success: function (partialView) {
+                $('#bodyId').removeClass('animated slideOutLeft');
+                $('#bodyId').fadeIn(500);
+                $('#bodyId').addClass('animated slideInUp');
+                $('#bodyId').html(partialView);
+            }
+        });
+    }, delayInMilliseconds);
+
+}
+
+
+
+
+function GoToUserSetUp() {
+    $('#bodyId').addClass('animated fadeOutUp');
+    $('#bodyId').fadeOut(300);
+
+    var delayInMilliseconds = 300; //1/2 of a second
+
+    setTimeout(function () {
+        $.ajax({
+            url: '../Login/UsernameSetUp',
+            type: "GET",
+            success: function (partialView) {
+                $('#bodyId').removeClass('animated slideOutLeft');
+                $('#bodyId').fadeIn(500);
+                $('#bodyId').addClass('animated slideInUp');
+                $('#bodyId').html(partialView);
+            }
+        });
+    }, delayInMilliseconds);
+
+}
+
+
+function AddressSetUp() {
+    $('#bodyId').addClass('animated fadeOutUp');
+    $('#bodyId').fadeOut(300);
+
+    var delayInMilliseconds = 300; //1/2 of a second
+
+    setTimeout(function () {
+        $.ajax({
+            url: '../Login/AddressSetUp',
+            type: "GET",
+            success: function (partialView) {
+                $('#bodyId').removeClass('animated slideOutLeft');
+                $('#bodyId').fadeIn(500);
+                $('#bodyId').addClass('animated slideInUp');
+                $('#bodyId').html(partialView);
+            }
+        });
+    }, delayInMilliseconds);
+
+}
